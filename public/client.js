b24d896d9cd4ebaf7d3be3ce5359189f650dcb1b
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-21 18:34:30 +0100

    Real-Time Strip Chart Recorder (Chart.js)
    Added CDN for Chart.js to index.html. Implemented pushing of local timestamps to tempChart labels. Implemented pushing of data.value to the chart dataset within telemetry_update. Added tempChart.update() method to render new data.

--- public/client.js
+++ public/client.js
@@ -1,5 +1,27 @@
 // Connects to the server hosting the page;
 
+const ctx = document.getElementById('tempChart').getContext('2d')
+const tempChart = new Chart(ctx , {
+    type: 'line',
+    data: {
+        labels: [], //Time stamps;
+        datasets: [{
+            label: ' Temperature (°C)',
+            data: [], //Live sensor values;
+            borderColor: '#ff4500',
+            borderWidth: 2,
+            fill: false
+        }]
+    },
+
+    options: {
+        scales: {
+            x: { display: true },
+            y: { suggestedMin: 10 , suggestedMax: 40 }
+        }
+    }
+})
+
 const socket = io()
 const tempValue = document.getElementById('temp-value')
 
@@ -8,4 +30,24 @@ socket.on('telemetry_update' , (data) => {
     if (tempValue) {
         tempValue.textContent = data.value
     }
+
+    //Current Timestamp on X axis (labels);
+
+    tempChart.data.labels.push(new Date().toLocaleTimeString())
+
+    //Add the temperature value received to dataset;
+
+    tempChart.data.datasets[0].data.push(data.value)
+
+    //Limite the points on the graph;
+
+    if (tempChart.data.labels.length > 20) {
+        tempChart.data.labels.shift()
+        tempChart.data.datasets[0].data.shift()
+    }
+
+    //Render the changes in the graph;
+
+    tempChart.update()
 })
+
